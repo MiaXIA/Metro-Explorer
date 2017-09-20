18de35efa1ee8efbdff024cf828b013265b4e213
mc.xia <summer.mc.xia@gmail.com>
2017-09-20 15:49:20 -0400

    增加了model文件夹以及下面的三个data文件，注释掉build.gradle下的几行compile

--- app/src/main/java/com/marks/metro/yichenzhou/metromarker/model/MetroStation.kt
+++ app/src/main/java/com/marks/metro/yichenzhou/metromarker/model/MetroStation.kt
@@ -6,3 +6,4 @@ package com.marks.metro.yichenzhou.metromarker.model
 
 data class MetroStation (val name:String, val longtitude: String, val latitude: String, val address: String, var color: Boolean)
 //按我的理解color是为了显示当前选取的是不是这个station，所以只要用Boolean应该就可以，也可以改成String保存颜色
+//test branch
