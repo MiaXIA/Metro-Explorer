57594b86ac9135f6d014a1e517e5d6864727fc10
YichenZhou <nightfallz@outlook.com>
2017-09-24 22:41:56 -0400

    Added fuel library Working on realm library integration

--- app/src/main/java/com/marks/metro/yichenzhou/metromarker/MenuActivity.kt
+++ app/src/main/java/com/marks/metro/yichenzhou/metromarker/MenuActivity.kt
@@ -2,11 +2,30 @@ package com.marks.metro.yichenzhou.metromarker
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.fuel.core.FuelManager
 
 class MenuActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        this.requestMetroList()
     }
+
+    private fun requestMetroList() {
+        FuelManager.instance.basePath = "https://api.wmata.com/Rail.svc/json/jStations"
+        FuelManager.instance.baseHeaders = mapOf("api_key" to "6f41a1603c704b57ba2e1e5741f79450")
+        Fuel.get("").response {
+            request, response, result ->
+            val (data, error) = result
+            if (error != null) {
+                println(error.toString())
+                return@response
+            }
+            println(data)
+        }
+    }
+
 }
