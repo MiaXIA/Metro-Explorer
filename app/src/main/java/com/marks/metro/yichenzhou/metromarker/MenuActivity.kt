a9dffe536d58d997a2315c995b04a8849772c2a1
YichenZhou <nightfallz@outlook.com>
2017-09-30 12:29:19 -0400

    Added metro station cached-base-preload mechanism.

--- app/src/main/java/com/marks/metro/yichenzhou/metromarker/MenuActivity.kt
+++ app/src/main/java/com/marks/metro/yichenzhou/metromarker/MenuActivity.kt
@@ -2,15 +2,30 @@ package com.marks.metro.yichenzhou.metromarker
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.marks.metro.yichenzhou.metromarker.helper.AppHelper
+import com.marks.metro.yichenzhou.metromarker.model.MetroStation
+import io.realm.Realm
+import kotlin.properties.Delegates
 
 class MenuActivity : AppCompatActivity() {
-
     val TAG = "MenuActivity"
+    var realm: Realm by Delegates.notNull()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
 
-        AppHelper.loadStationsData("station.csv", applicationContext)
+        Realm.init(applicationContext)
+        this.realm = Realm.getDefaultInstance()
+
+        val stationCount = this.realm.where(MetroStation::class.java).findAll().count()
+        if (stationCount== 0) {
+            Log.d(TAG, "No metro station data, working on it now")
+            AppHelper.loadStationsData("Stations.csv", applicationContext)
+            Log.d(TAG, "Done")
+        } else {
+            Log.d(TAG, "Metro station data exists, count: ${stationCount.toString()}")
+        }
     }
 }
--- app/src/main/java/com/marks/metro/yichenzhou/metromarker/helper/AppHelper.kt
+++ app/src/main/java/com/marks/metro/yichenzhou/metromarker/helper/AppHelper.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.res.AssetManager
 import android.util.Log
 import com.marks.metro.yichenzhou.metromarker.model.MetroStation
+import io.realm.Realm
 import java.nio.charset.Charset
 
 /**
@@ -13,7 +14,7 @@ import java.nio.charset.Charset
 object AppHelper {
     val TAG = "AppHelper"
     // Extension for AssetManager
-    fun AssetManager.fileAsString(fileName: String): String {
+    private fun AssetManager.fileAsString(fileName: String): String {
         return open(fileName).use {
             it.readBytes().toString(Charset.defaultCharset())
         }
@@ -21,15 +22,23 @@ object AppHelper {
 
     fun loadStationsData(fileName: String, context: Context): MetroStation? {
         try {
+            val realm = Realm.getDefaultInstance()
             val csvStr = context.assets.fileAsString(fileName)
-            val lines = csvStr.split(";".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+            val lines = csvStr.split(";\n".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
             for (line in lines) {
-                Log.d(TAG, line)
+                val units = line.split(",")
+                //Make sure data format is correct
+                if (units.count() != 7) {
+                    throw  Exception("Invalid data format")
+                }
+                realm.executeTransaction {
+                    val station = realm.createObject(MetroStation::class.java)
+                    station.setValues(units)
+                }
             }
         } catch (e: Exception) {
             Log.e(TAG, e.message)
         }
         return  null
     }
-
 }
