82429fa0b92b749275c122b62782d02b3b2001a2
YichenZhou <nightfallz@outlook.com>
2017-09-30 13:24:01 -0400

    Added function that fetch device's last known location removed fuel git repo

--- app/src/main/java/com/marks/metro/yichenzhou/metromarker/MenuActivity.kt
+++ app/src/main/java/com/marks/metro/yichenzhou/metromarker/MenuActivity.kt
@@ -1,23 +1,49 @@
 package com.marks.metro.yichenzhou.metromarker
 
+
+import android.content.Context
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.marks.metro.yichenzhou.metromarker.helper.AppHelper
 import com.marks.metro.yichenzhou.metromarker.model.MetroStation
 import io.realm.Realm
+import kotlinx.android.synthetic.main.activity_menu.*
 import kotlin.properties.Delegates
 
 class MenuActivity : AppCompatActivity() {
-    val TAG = "MenuActivity"
-    var realm: Realm by Delegates.notNull()
+    private val TAG = "MenuActivity"
+    private var realm: Realm by Delegates.notNull()
+    private var locationManager: LocationManager? = null
+
+    private val locationListener: LocationListener = object : LocationListener {
+        override fun onLocationChanged(location: Location) {
+            Log.d(TAG, "(Long:Lang): " + location.longitude + " : " + location.latitude)
+        }
+        override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
+        override fun onProviderEnabled(provider: String) {}
+        override fun onProviderDisabled(provider: String) {}
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
 
+        // Properties Initialization
         Realm.init(applicationContext)
         this.realm = Realm.getDefaultInstance()
+        this.locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager?
+
+        locationTestButton.setOnClickListener {
+            try {
+                this.locationManager?.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0L, 0f, locationListener)
+            } catch (e: SecurityException) {
+                Log.e(TAG, e.message)
+            }
+        }
 
         val stationCount = this.realm.where(MetroStation::class.java).findAll().count()
         if (stationCount== 0) {
