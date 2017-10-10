eb45510cff2f0f49956cf0ad4032cb06cc969879
YichenZhou <nightfallz@outlook.com>
2017-10-10 12:06:58 -0400

    Added permission status checker

--- app/src/main/java/com/marks/metro/yichenzhou/metromarker/MenuActivity.kt
+++ app/src/main/java/com/marks/metro/yichenzhou/metromarker/MenuActivity.kt
@@ -1,11 +1,14 @@
 package com.marks.metro.yichenzhou.metromarker
 
 import android.content.Context
+import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.util.Log
 import com.google.android.gms.location.places.GeoDataClient
 import com.google.android.gms.location.places.Place
@@ -30,34 +33,6 @@ class MenuActivity : AppCompatActivity(), OnMapReadyCallback {
     private var latitude: Double = 0.0
     private var longitude: Double = 0.0
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_menu)
-
-        // Properties Initialization
-        Realm.init(applicationContext)
-        this.realm = Realm.getDefaultInstance()
-        this.locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager?
-        this.mapFragment = supportFragmentManager.findFragmentById(R.id.mapView) as SupportMapFragment?
-
-        try {
-            // We use location manager to fetch current location
-            this.locationManager?.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0L, 0f, locationListener)
-        } catch (e: SecurityException) {
-            Log.e(TAG, e.message)
-        }
-    }
-
-    override fun onMapReady(p0: GoogleMap?) {
-        val location = LatLng(this.latitude, this.longitude)
-        if (p0 !is GoogleMap) {
-            Log.e(TAG, "Invalid type for p0")
-            return
-        }
-        p0.addMarker(MarkerOptions().position(location).title("My Location"))
-        p0.moveCamera(CameraUpdateFactory.newLatLng(location))
-    }
-
     private val locationListener: LocationListener = object : LocationListener {
         override fun onLocationChanged(location: Location) {
             latitude = location.latitude
@@ -75,6 +50,58 @@ class MenuActivity : AppCompatActivity(), OnMapReadyCallback {
         override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
         override fun onProviderEnabled(provider: String) {}
         override fun onProviderDisabled(provider: String) {}
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_menu)
+
+        this.requestPermission()
+
+        // Properties Initialization
+        Realm.init(applicationContext)
+        this.realm = Realm.getDefaultInstance()
+        this.locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager?
+        this.mapFragment = supportFragmentManager.findFragmentById(R.id.mapView) as SupportMapFragment?
+
+        if (AppHelper.checkPermissionStatus(AppHelper.LOCATION_DEFAULT_CODE, this)) {
+            try {
+                this.locationManager?.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0L, 0f, locationListener)
+            } catch (e: SecurityException) {
+                Log.e(TAG, e.message)
+            }
+        }
+    }
+
+    override fun onMapReady(p0: GoogleMap?) {
+        val location = LatLng(this.latitude, this.longitude)
+        if (p0 !is GoogleMap) {
+            Log.e(TAG, "Invalid type for p0")
+            return
+        }
+        p0.addMarker(MarkerOptions().position(location).title("My Location"))
+        p0.moveCamera(CameraUpdateFactory.newLatLng(location))
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == AppHelper.LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults.first() != PackageManager.PERMISSION_GRANTED) {
+                this.requestPermission()
+            }
+        }
+        // Where to fetch location again after location permission granted
+        try {
+            this.locationManager?.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0L, 0f, locationListener)
+        } catch (e: SecurityException) {
+            Log.e(TAG, e.message)
+        }
+    }
+
+    private fun requestPermission() {
+        if (!AppHelper.checkPermissionStatus(AppHelper.LOCATION_DEFAULT_CODE, this)) {
+            ActivityCompat.requestPermissions(this, arrayOf(AppHelper.LOCATION_DEFAULT_CODE), AppHelper.LOCATION_PERMISSION_REQUEST_CODE)
+        }
     }
 }
 
