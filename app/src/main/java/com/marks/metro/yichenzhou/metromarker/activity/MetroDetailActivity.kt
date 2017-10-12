8d4aedbf4c76d9967ed75f149d05a9dea071778d
McX <summer.mc.xia@gmail.com>
2017-10-12 15:39:25 -0400

    clear icon and new files update

---
+++ app/src/main/java/com/marks/metro/yichenzhou/metromarker/activity/MetroDetailActivity.kt
@@ -0,0 +1,16 @@
+package com.marks.metro.yichenzhou.metromarker.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.marks.metro.yichenzhou.metromarker.R
+
+class MetroDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.metro_detail)
+
+        //TODO
+        //set listview adapter and clicklistener
+    }
+}
